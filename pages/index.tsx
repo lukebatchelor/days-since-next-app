fa90d6fa228bfc0c1acb901dec19a1c4587c8a79
Luke.Batchelor <batchelor.luke+git@gmail.com>
2021-07-04 13:29:07 +1000

    Quick landing page to test deployments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { User } from '.prisma/client';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [checkins, setCheckins] = useState<Array<User>>([]);
+  useEffect(() => {
+    fetch('/api/users')
+      .then((res) => res.json())
+      .then((r) => setCheckins(r.users));
+  }, []);
   return (
     <div className={styles.container}>
-     
-
-      
+      {' '}
+      Hello World
+      <div>{checkins && checkins.map((checkin) => <div key={checkin.id}>{JSON.stringify(checkin)}</div>)}</div>
     </div>
-  )
+  );
 }
